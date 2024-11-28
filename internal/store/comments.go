bd9ff665212ae28a56a2f0da0f87f2aca3e3a28d
g.sarbaj@icloud.com <g.sarbaj@icloud.com>
2024-11-28 12:21:21 +0200

    adding comments to posts

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -3,7 +3,6 @@ package store
 import (
 	"context"
 	"database/sql"
-	"log"
 )
 
 type Comment struct {
@@ -12,12 +11,39 @@ type Comment struct {
 	UserID    int64  `json:"user_id"`
 	Content   string `json:"content"`
 	CreatedAt string `json:"created_at"`
+	User      User   `json:"user"`
 }
 
-type CommentsSore struct {
+type CommentSore struct {
 	db *sql.DB
 }
 
-func (s *CommentsSore) GetByPostID(ctx context.Context, postID int64) (*Post, error) {
-	log.Printf("GetByPostID: postID=%d", postID)
+func (s *CommentSore) GetByPostID(ctx context.Context, postID int64) ([]Comment, error) {
+
+	query := `
+		SELECT c.id, c.post_id, c.user_id, c.content, c.created_at, users.username, users.id FROM comments c
+		JOIN users on users.id = c.user_id
+		WHERE c.post_id = $1
+		ORDER BY c.created_at DESC  
+	`
+
+	rows, err := s.db.QueryContext(ctx, query, postID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	comments := []Comment{}
+	for rows.Next() {
+		var c Comment
+		c.User = User{}
+		err := rows.Scan(&c.ID, &c.PostID, &c.UserID, &c.Content, &c.CreatedAt, &c.User.Username, &c.User.ID)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+
+	return comments, nil
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -15,11 +15,11 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-type UsersStore struct {
+type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context, user *User) error {
+func (s *UserStore) Create(ctx context.Context, user *User) error {
 	query := ` INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, created_at`
 
 	err := s.db.QueryRowContext(ctx, query, user.Username, user.Email, user.Password).Scan(&user.ID, &user.CreatedAt)
