4ee0fdac69241a8b34d211a95b0bc69f425bc8c6
g.sarbaj@icloud.com <g.sarbaj@icloud.com>
2024-12-09 11:42:59 +0200

    user get by id

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -61,6 +61,15 @@ func (app *application) mount() http.Handler {
 	})
 
 	//users
+
+	r.Route("/v1/users", func(r chi.Router) {
+		r.Route("/{userID}", func(r chi.Router) {
+			r.Get("/", app.getUserHandler)
+
+		})
+
+	})
+
 	//auth
 
 	return r
